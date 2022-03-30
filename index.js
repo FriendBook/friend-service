a0dc29eb05bdbc1289451ce86f5d047969984c7c
jnavacar <jnavacar1998@gmail.com>
2022-03-30 16:41:13 +0200

    created endpoints

--- index.js
+++ index.js
@@ -1,7 +1,58 @@
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const PORT = 8082;
 
 app.use(express.json())
 
 app.listen(PORT, () => console.log('It\'s alive on: http://localhost:' + PORT))
+
+let connections = {
+    1: [2,3,4,5,6,7,8,9],
+    2: [1,4,5],
+    3: [1,5,7]
+};
+
+//Get all friends from a user
+app.get('/friends/:id', (req, res) => {
+    if(!!connections[req.params.id]) {
+        res.status(200).send(connections[req.params.id])
+        return
+    }
+    else
+    {
+        throw 'The user with ID ' + req.params.id + ' does not exist.'
+        return
+    }
+});
+
+//Check if user has a specific friend
+app.get('/friends/:id/:friendid', (req, res) => {
+    if(!!connections[req.params.id]) {
+        res.status(200).send(connections[req.params.id].includes(parseInt(req.params.friendid)))
+        return
+    }
+    else
+    {
+        throw 'The user with ID ' + req.params.id + ' does not exist.'
+        return
+    }
+});
+
+app.delete('/friends/:id/:friendid', (req, res) => {
+    if(!!connections[req.params.id]) {
+        if(connections[req.params.id].includes(parseInt(req.params.friendid))) {
+            connections[req.params.id].splice(connections[req.params.id].indexOf(parseInt(req.params.friendid)), 1)
+            res.status(200).send(connections[req.params.id])
+        }
+        else
+        {
+            throw 'The user does not have this person as friend.'
+            return
+        }
+    }
+    else
+    {
+        throw 'The user with ID ' + req.params.id + ' does not exist.'
+        return
+    }
+})
